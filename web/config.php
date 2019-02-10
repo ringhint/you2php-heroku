e7396b5ea6a5da92fee80c946d0afdb7d76b244b
ringhint <47498421+ringhint@users.noreply.github.com>
2019-02-10 21:14:44 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyCtQigBC6Jm_ThZkXVE8sw95DdcKZXEVKg');//这里改成您的api 
-define('GJ_CODE', 'KR');//国家代码
+define('GJ_CODE', 'JP');//国家代码
 define('SITE_NAME', 'Youtube');//网站名字
 define('TITLENAME', 'Youtube');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('EN2DEKEY', 'cxQUP1XDwd435e67CszDSh4');//一个随机字符串，加解密KEY
+define('EMAIL', '2391615516@qq.com');//邮箱
 ?>
